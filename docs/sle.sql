f0daf4c4d06e52723bfc740649f443fc1bb79fb1
myluguo <luguo@139.com>
2014-06-25 18:56:41 +0800

    模型编码

--- docs/sle.sql
+++ docs/sle.sql
@@ -1,4 +1,4 @@
-create database sle collate  utf8_general_ci default character set utf8;
+create database if not exists sle collate  utf8_general_ci default character set utf8;
 
-
-create table sle_user(id int auto_increment primary key,name varchar(50) not null collate utf8_general_ci  default '',pwd varchar(255) not null  collate utf8_general_ci default '',`desc` varchar(255) collate utf8_general_ci default '' comment '简介')engine=innodb default character set utf8  collate utf8_general_ci;
+use sle;
+create table  if not exists  sle_user(id int auto_increment primary key,name varchar(50) not null collate utf8_general_ci  default '',pwd varchar(255) not null  collate utf8_general_ci default '',`desc` varchar(255) collate utf8_general_ci default '' comment '简介')engine=innodb default character set utf8  collate utf8_general_ci;
